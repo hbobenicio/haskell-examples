f0d88265733c8440b494e93f464bb4626b7412a9
Hugo Benício <hbobenicio@gmail.com>
2017-07-02 15:10:35 -0300

    Providing type aliases for positional-parser example

--- positional-parser/positional-parser.hs
+++ positional-parser/positional-parser.hs
@@ -1,15 +1,19 @@
 import Data.List (intersperse)
 import Control.Arrow ((>>>))
 
+type Field = String
+type FieldList = [Field]
+type ConsumeStream = (FieldList, String)
+
 -- Just prepares the datastructure for line processing
-prepare :: String -> ([String], String)
+prepare :: String -> ConsumeStream
 prepare linha = ([], linha)
 
 -- Consuming Logic:
 --   1. Splits the line on determined position N
 --   2. Appends it to the resulting list (at the head, yielding in a reversed list of fields!)
 --   3. Returns the same datastructure for future processings through function composition
-consume :: Int -> ([String], String) -> ([String], String)
+consume :: Int -> ConsumeStream -> ConsumeStream
 consume n (fields, line) =
   let
     (field, consumedLine) = splitAt n line
@@ -18,7 +22,7 @@ consume n (fields, line) =
 
 -- Finishes the parsing by reversing the reversed list got from consumption
 -- Returns the fields list in the right order
-finish :: ([String], String) -> [String]
+finish :: ConsumeStream -> FieldList
 finish (reversedFields, _) = reverse reversedFields
 
 -- Using Currying for customizing
