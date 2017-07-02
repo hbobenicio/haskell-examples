4a301170283c731f907f5c6f9076d59887cc9d9a
Hugo Benício <hbobenicio@gmail.com>
2017-07-02 13:47:52 -0300

    Improving positional-parser example

--- positional-parser/positional-parser.hs
+++ positional-parser/positional-parser.hs
@@ -1,16 +1,25 @@
-import Data.List
+import Data.List (intersperse)
+import Control.Arrow ((>>>))
 
--- Just preparing the datastructure for line processing
+-- Just prepares the datastructure for line processing
 prepare :: String -> ([String], String)
 prepare linha = ([], linha)
 
 -- Consuming Logic:
 --   1. Splits the line on determined position N
---   2. Appends it to the resulting list
---   3. Returns the same datastructure for future processings through composition
+--   2. Appends it to the resulting list (at the head, yielding in a reversed list of fields!)
+--   3. Returns the same datastructure for future processings through function composition
 consume :: Int -> ([String], String) -> ([String], String)
-consume n (fields, line) = (fields ++ [field], consumedLine)
-  where (field, consumedLine) = splitAt n line
+consume n (fields, line) =
+  let
+    (field, consumedLine) = splitAt n line
+  in
+    (field : fields, consumedLine)
+
+-- Finishes the parsing by reversing the reversed list got from consumption
+-- Returns the fields list in the right order
+finish :: ([String], String) -> [String]
+finish (reversedFields, _) = reverse reversedFields
 
 -- Using Currying for customizing
 -- each field size
@@ -18,9 +27,15 @@ consumeFirst = consume 5
 consumeSecond = consume 6
 consumeThird = consume 6
 
+-- Composing the parsing function.
+-- The (>>>) operator works like the inverse order of function composition operator (.)
+-- In other words: (g . f) == (f >>> g)
+-- This works similar to F#, Elixir's and Elm Pipe Operator '|>'
+parseLine = prepare >>> consumeFirst >>> consumeSecond >>> consumeThird >>> finish
+
 main = do
   let line = "25101170007089280"
-  let (fields, _) = (consumeThird . consumeSecond . consumeFirst) (prepare line)
-  let finalFields = (intersperse ";" fields) ++ ["\n"]
-  mapM putStr finalFields
+  let fields =  parseLine line -- This yields to a list of fields :: [String]
+  let csvFields = intersperse ";" fields -- Interpolates ";" between fields
+  mapM putStr (csvFields ++ ["\n"]) -- Print every field and a line feed at the end
 
