cc848e7042e3baf6e2c765f0472f0c678729cc30
Hugo Benício <hbobenicio@gmail.com>
2017-08-25 07:17:57 -0300

    Printing the PATH list with index

--- check-path/src/Main.hs
+++ check-path/src/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
 import System.Environment (getEnv)
-import Data.List.Split (splitOn)
+import Data.List.Split (splitOn) -- From the 'split' library
 
 expandPath :: String -> [String]
 expandPath path = splitOn ":" path
 
 putPathLn :: [String] -> IO ()
-putPathLn pathList = mapM_ putStrLn pathList
+putPathLn pathList = mapM_ putStrLn formattedPathList
+  where
+    indexedPathList = zip [1..] pathList
+    formattedPathList = map (\(i,s) -> show i ++ ". " ++ s) indexedPathList
 
 main :: IO ()
 main = do
