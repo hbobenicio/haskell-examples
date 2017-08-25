9f12753ea7fd674d5bfc427eb86a3b49b743b696
Hugo Benício <hbobenicio@gmail.com>
2017-08-25 08:00:07 -0300

    Checking for duplicates done

--- check-path/src/Main.hs
+++ check-path/src/Main.hs
@@ -1,10 +1,15 @@
 module Main where
 
 import System.Environment (getEnv)
+import Data.List (sort)
 import Data.List.Split (splitOn) -- From the 'split' library
+import qualified Data.Set as Set -- From the 'containers' library
 
 expandPath :: String -> [String]
 expandPath path = splitOn ":" path
+
+expandPathAndSort :: String -> [String]
+expandPathAndSort path = sort $ expandPath path
 
 putPathLn :: [String] -> IO ()
 putPathLn pathList = mapM_ putStrLn formattedPathList
@@ -12,7 +17,14 @@ putPathLn pathList = mapM_ putStrLn formattedPathList
     indexedPathList = zip [1..] pathList
     formattedPathList = map (\(i,s) -> show i ++ ". " ++ s) indexedPathList
 
+hasDuplicates :: (Ord a) => [a] -> Bool
+hasDuplicates list = length list /= length set
+  where set = Set.fromList list
+
 main :: IO ()
 main = do
   path <- getEnv "PATH"
-  (putPathLn . expandPath) path
+  let pathList = expandPath path
+  putPathLn pathList
+  putStrLn ""
+  putStrLn (if hasDuplicates pathList then "Your PATH has duplicates!" else "Your PATH has no duplicates.")
