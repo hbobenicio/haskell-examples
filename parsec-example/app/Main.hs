90849de9c8d286651b637d27c430fd13a7f78dd9
Hugo Benício <hbobenicio@gmail.com>
2017-09-05 06:39:30 -0300

    Adding support for input file argument

--- parsec-example/app/Main.hs
+++ parsec-example/app/Main.hs
@@ -2,11 +2,16 @@ module Main where
 
 import ParsecExample
 
+import System.Environment
+import System.Exit
+
 main :: IO ()
 main = do
-  contents <- getContents
+  args <- getArgs
+  contents <- if null args then getContents else readFile $ head args
   case parsePositionalFile contents of
-    Right registers -> mapM_ (putStrLn . show) registers
+    Right registers ->
+      mapM_ print registers
     Left e -> do
       putStrLn "Parsing Error:"
       print e
