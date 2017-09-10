fa5c93d5362a5137fd242ccaf814121e9fba56e2
Hugo Benício <hbobenicio@gmail.com>
2017-09-10 20:52:14 -0300

    Finishing check-path primary feature

--- check-path/src/Main.hs
+++ check-path/src/Main.hs
@@ -3,35 +3,67 @@ module Main where
 import System.Environment (getEnv)
 import Data.List (sort)
 
-import qualified Data.Map as Map -- From the 'containers' library
+-- import qualified Data.Map as Map -- From the 'containers' library
 import Data.List.Split (splitOn) -- From the 'split' library
 import qualified Data.Set as Set -- From the 'containers' library
 
-import qualified Checkpath.Histogram as Histogram
+-- import qualified Checkpath.Histogram as Histogram
+import qualified Data.Map.Strict as Map -- From the 'containers' library
+
+import Data.Maybe (fromMaybe) -- Same behaviour as Optional::orElse from java
+
+type Histogram a = Map.Map a Int
 
 type Path = String
-type PathHistogram = Histogram.Histogram Path
+type PathHistogram = Histogram Path
+type PathHistogramItem = (String, Int)
+
+pathHistogramLookup :: Path -> PathHistogram -> PathHistogramItem
+pathHistogramLookup path histogram = (path, count)
+  where
+    count = fromMaybe 0 (Map.lookup path histogram)
+
+showPathHistogramItem :: PathHistogramItem -> String
+showPathHistogramItem (path, count) =
+  if count == 1 then path else path ++ " [!!]"
+
+showPath :: Path -> PathHistogram -> String
+showPath path histogram = showPathHistogramItem $ pathHistogramLookup path histogram
+
+printPathHistogramItem :: PathHistogramItem -> IO ()
+printPathHistogramItem = putStrLn . showPathHistogramItem
 
 expandPath :: String -> [Path]
-expandPath path = splitOn ":" path
+expandPath = splitOn ":"
 
-expandPathAndSort :: String -> [Path]
-expandPathAndSort path = sort $ expandPath path
-
-putPathLn :: [Path] -> IO ()
-putPathLn pathList = mapM_ putStrLn formattedPathList
+putPathLn :: PathHistogram -> [Path] -> IO ()
+putPathLn pathHistogram pathList = mapM_ putStrLn formattedPathList
   where
     indexedPathList = zip [1..] pathList -- :: [(Int, Path)]
-    formattedPathList = map (\(i,s) -> show i ++ ". " ++ s) indexedPathList
+    formattedPathList = map (\(i,s) -> show i ++ ". " ++ showPath s pathHistogram) indexedPathList
+
+putPathHistogramLn :: PathHistogram -> IO ()
+putPathHistogramLn pathHistogram = mapM_ printPathHistogramItem histogramList
+  where
+    histogramList = Map.assocs pathHistogram
 
 hasDuplicates :: (Ord a) => [a] -> Bool
 hasDuplicates list = length list /= length set
   where set = Set.fromList list
 
+histogramFromPathList :: [Path] -> PathHistogram
+histogramFromPathList list = histogramFromPathList' list Map.empty
+
+histogramFromPathList' :: [Path] -> PathHistogram -> PathHistogram
+histogramFromPathList' [] histogram = histogram
+histogramFromPathList' (x:xs) histogram =
+  Map.insertWith (+) x 1 (histogramFromPathList' xs histogram)
+
 main :: IO ()
 main = do
   path <- getEnv "PATH"
   let pathList = expandPath path
-  putPathLn pathList
+  let pathHistogram = histogramFromPathList pathList
+  putPathLn pathHistogram pathList
   putStrLn ""
   putStrLn (if hasDuplicates pathList then "Your PATH has duplicates!" else "Your PATH has no duplicates.")
